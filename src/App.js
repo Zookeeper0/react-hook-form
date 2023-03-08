66475835bad4b114ce8ae0f8c75c5e4ec3053e0b
eastzoo <dongju010@kakao.com>
2023-03-08 23:59:41 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Input from "./Input";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object({
+  username: yup.string().required("Username is a required field"),
+  phoneNumber: yup.string().required("Phone number is a required field").matches(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/, "Phone number is not valid!."),
+  email: yup.string().required("Email is a required field").email("Email is not valid!."),
+  password: yup.string().min(6, "Password must be at least 6 characters"),
+  confirmPassword: yup.string().oneOf([yup.ref("password")], "Password must be match.")
+})
 
 function App() {
+  const { handleSubmit, register, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  console.log(errors)
+
+  const formSubmit = (data) => {
+    console.log(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="sign-up">
+      <h1>Sign Up</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur
+      </p>
+      <form onSubmit={handleSubmit(formSubmit)}>
+        <Input id="username" label="Username" type="text" placeholder="Enter Username" register={{ ...register("username") }} errorMessage={errors.username?.message} />
+        <Input id="phoneNumber" label="Phone Number" type="text" placeholder="Enter Email" register={{ ...register("phoneNumber") }} errorMessage={errors.phoneNumber?.message} />
+        <Input id="email" label="Email" type="email" placeholder="Enter Username" register={{ ...register("email") }} errorMessage={errors.email?.message} />
+        <Input id="password" label="Password" type="password" placeholder="Enter Username" register={{ ...register("password") }} errorMessage={errors.password?.message} />
+        <Input id="confirmPassword" label="Confirm Password" type="password" placeholder="Confirm Password" register={{ ...register("confirmPassword") }} errorMessage={errors.confirmPassword?.message} />
+        <button>Sign Up</button>
+      </form>
     </div>
   );
 }
